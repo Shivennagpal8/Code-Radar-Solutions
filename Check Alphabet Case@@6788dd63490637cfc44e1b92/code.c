7496b9de8ae58424ef8abeaa8edfcd56371ef243
Shiven Nagpal <shivennagpal8@gmail.com>
2025-02-07 16:50:55 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -1,38 +1,34 @@
 #include <stdio.h>
 
-// int main() {
-//     char ch;
-//     scanf("%c", &b);
-//     if (('A'<=ch)&&(ch<='Z')){
-//         printf("Uppercase");
-//     }
-//     else if(('a'<=ch)&&(ch<='z')){
-//         printf("Lowercase");
-//     }
-//     else{
-//         printf("Not an alphabet");
-//     }
-//     return 0;
-// }
-
 int main() {
     char ch;
-
-    scanf("%c", &ch);
-
-    if(ch >='A' && ch<='Z'){
+    scanf("%c", &b);
+    if (ch>='A')&&(ch<='Z'){
         printf("Uppercase");
     }
-    else if (ch >= 'a' && ch<='z'){
+    else if((ch>='a')&&(ch<='z')){
         printf("Lowercase");
     }
     else{
         printf("Not an alphabet");
     }
-
-
-    /////if (ch >="a"&& )
-    /////ch >= 97 and ch>= "a" is same this as it is its ascia value
-
     return 0;
 }
+
+// int main() {
+//     char ch;
+
+//     scanf("%c", &ch);
+
+//     if(ch >='A' && ch<='Z'){
+//         printf("Uppercase");
+//     }
+//     else if (ch >= 'a' && ch<='z'){
+//         printf("Lowercase");
+//     }
+//     else{
+//         printf("Not an alphabet");
+//     }
+
+//     return 0;
+// }
