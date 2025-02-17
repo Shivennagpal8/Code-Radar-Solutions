97a1c24d06d32ca9179fe2289be631e35913bafa
Shiven Nagpal <shivennagpal8@gmail.com>
2025-02-17 20:25:08 +0530

    Solution to Grade Based on Marks

---
+++ Grade Based on Marks@@6788dd63490637cfc44e1b96/code.c
@@ -0,0 +1,19 @@
+// Your code here...
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    switch{
+        case (a>=90):
+            printf("A");
+        case (a>=80 && a<90):
+            printf("B");
+        case (a>=70 && a<80):
+            printf("C");
+        case (a>=60 && a<70):
+            printf("D");
+        case (a<60):
+            printf("F");
+
+    }
+}
